c7c37b6f1b059abf04f2734ff20703bc7bd54dc3
Konstelllation <harchenkobo2003@gmail.com>
2022-06-02 23:07:19 +0300

    fixed inds

--- ind/ind1.py
+++ ind/ind1.py
@@ -9,16 +9,23 @@ if __name__ == '__main__':
     s_g = 0
     s_f = 0
 
+    # Ввод оценок
     n = int(input('Введите количество учащихся: '))
     for i in range(n):
         print('\n', i + 1, '-й учащийся')
         A.append(int(input('Введите оценку за алгебру: ')))
         G.append(int(input('Введите оценку за геометрию: ')))
         F.append(int(input('Введите оценку за физику: ')))
-        s_a += A[i]
-        s_g += G[i]
-        s_f += F[i]
 
+    # Подсчет суммы баллов по каждому предмету
+    for i, x in enumerate(A):
+        s_a += x  # Сумма баллов по алгебре
+    for i, x in enumerate(G):
+        s_g += x  # Сумма баллов по геометрии
+    for i, x in enumerate(F):
+        s_f += x  # Сумма баллов по физике
+
+    # Вывод лучшей успеваемости по предмету
     if s_a > s_g and s_a > s_f:
         print('\nЛучше всего успеваемость по алгебре')
     elif s_g > s_a and s_g > s_f:
--- ind/ind2.py
+++ ind/ind2.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-
-
 if __name__ == '__main__':
     li = []
     s = 0
     mn = 0
     mx = 0
     pr = 1
+
+    # Ввод элементов
     n = int(input('Введите количество переменных: '))
     for i in range(n):
         li.append(int(input()))
-        if i == 0:
-            mn = li[i]
-            mx = li[i]
-        if li[i] > 0:
-            s += li[i]
-        if abs(li[i]) < mn:
+
+    # Определение суммы положит. эл. и индексы макс. и мин. эл. строки
+    for i, x in enumerate(li):
+        if x > 0:
+            s += x
+        if abs(x) < li[mn]:
             mn = i
-        if abs(li[i]) > mx:
+        if abs(x) > li[mx]:
             mx = i
+
+    # Произведение эл. между макс. и мин. эл.
     if mx < mn:
         for i in range(mx + 1, mn):
             pr *= li[i]
     else:
         for i in range(mn + 1, mx):
             pr *= li[i]
+
+    # Вывод s, pr, и отсортированного списка
     print('Сумма положительных элементов списка = ', s)
     print('Произведение элементов списка, расположенных между максимальным'
           ' по модулю и минимальным по модулю элементами = ', pr)
